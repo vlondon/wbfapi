56edbd80e206fff902c9b17696d228a04a2adcc8
Vladimirs Matusevics <vladimir.matusevic@gmail.com>
2015-08-01 19:39:52 +0100

    Error message added for the failed call.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -13,42 +13,64 @@ angular.module('testApp')
     $scope.loading = false;
     $scope.venues = [];
     $scope.groups = [];
+    $scope.error = undefined;
 
     var that = this;
 
-    that.searchVenue = function() {
+    that.searchVenue = function () {
       var searchLocation = $scope.searchForm.location.$viewValue;
       var searchQuery = $scope.searchForm.query.$viewValue;
 
       // Reset results
       $scope.venues = [];
       $scope.groups = [];
+      $scope.error = undefined;
 
       // Show loading
       $scope.loading = true;
 
-      foursquareApi.venues.searchVenue(searchLocation, searchQuery).then(function (data) {
-        $scope.loading = false;
-        console.log('data: ', data);
+      foursquareApi.venues.searchVenue(searchLocation, searchQuery)
+        .then(function (data) {
+          // Request succeed: Display results
+          $scope.loading = false;
+          console.log('SUCCEED data: ', data);
 
-        if(data.venues) {
-          console.log('Show search results: ', data.venues);
-          $scope.venues = data.venues;
-        } else if (data.groups && data.groups.length > 0) {
-          $scope.groups = data.groups;
-          data.groups.forEach( function(group) {
-            console.log('Show group: ', group.name, group.items);
-          });
-        }
-        if(!$scope.$$phase) {
-          $scope.$digest();
-        }
+          if (data.venues) {
+            console.log('Show search results: ', data.venues);
+            $scope.venues = data.venues;
+          } else if (data.groups && data.groups.length > 0) {
+            $scope.groups = data.groups;
+            data.groups.forEach(function (group) {
+              console.log('Show group: ', group.name, group.items);
+            });
+          }
+          if (!$scope.$$phase) {
+            $scope.$digest();
+          }
 
-      });
+        })
+        .catch(function(data){
+          // Request failed: Display error message
+          $scope.loading = false;
+          console.log('FAILED data: ', data);
+
+          var error = {
+            type: 0,
+            message: 'Search failed. Please try again.'
+          };
+
+          var errorReason = 'Unknown';
+          if (data.meta && data.meta.errorDetail) {
+            errorReason = data.meta.errorDetail;
+          }
+          error.message += ' ( Reason: ' + errorReason + ' )';
+
+          $scope.error = error;
+        });
     };
 
 
-    $scope.searchFormSubmit = function() {
+    $scope.searchFormSubmit = function () {
       if ($scope.searchForm.$valid) {
 
         that.searchVenue();
