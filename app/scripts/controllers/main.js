f2a97e614e2c4425cfa73f7af6ea4ff49fc52928
Vladimirs Matusevics <vladimir.matusevic@gmail.com>
2015-08-01 18:59:52 +0100

    Search form added. Displaying results.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,17 +8,55 @@
  * Controller of the testApp
  */
 angular.module('testApp')
-  .controller('MainCtrl', function (foursquareApi) {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('MainCtrl', function ($scope, foursquareApi) {
 
-    foursquareApi.venues.searchVenue('London', 'donuts').then(function (data) {
+    $scope.loading = false;
+    $scope.venues = [];
+    $scope.groups = [];
 
-      console.log('data: ', data);
+    var that = this;
 
-    });
+    that.searchVenue = function() {
+      var searchLocation = $scope.searchForm.location.$viewValue;
+      var searchQuery = $scope.searchForm.query.$viewValue;
+
+      // Reset results
+      $scope.venues = [];
+      $scope.groups = [];
+
+      // Show loading
+      $scope.loading = true;
+
+      foursquareApi.venues.searchVenue(searchLocation, searchQuery).then(function (data) {
+        $scope.loading = false;
+        console.log('data: ', data);
+
+        if(data.venues) {
+          console.log('Show search results: ', data.venues);
+          $scope.venues = data.venues;
+        } else if (data.groups && data.groups.length > 0) {
+          $scope.groups = data.groups;
+          data.groups.forEach( function(group) {
+            console.log('Show group: ', group.name, group.items);
+          });
+        }
+        if(!$scope.$$phase) {
+          $scope.$digest();
+        }
+
+      });
+    };
+
+
+    $scope.searchFormSubmit = function() {
+      if ($scope.searchForm.$valid) {
+
+        that.searchVenue();
+
+        return true;
+      } else {
+        return false;
+      }
+    };
 
   });
--- app/scripts/factories/api/foursquareApi.js
+++ app/scripts/factories/api/foursquareApi.js
@@ -4,11 +4,13 @@ angular.module('testApp')
 
     var apiConfig = {
       domain: 'https://api.foursquare.com/v2',
-      baseParams: {
-        client_id: 'L1VH4GBLNWJO5GC51EGLYXGVQQFCL1P322GNELHE5AORKLKD',
-        client_secret: 'PJAIAIK5W4YMBB5M24FNG2JJMR50NR3TASEQA13VYIPTLWMD',
-        v: 20140806,
-        m: 'foursquare'
+      baseParams: function() {
+        return {
+          client_id: 'L1VH4GBLNWJO5GC51EGLYXGVQQFCL1P322GNELHE5AORKLKD',
+          client_secret: 'PJAIAIK5W4YMBB5M24FNG2JJMR50NR3TASEQA13VYIPTLWMD',
+          v: 20140806,
+          m: 'foursquare'
+        }
       }
     };
 
@@ -17,14 +19,14 @@ angular.module('testApp')
 
     // --- VENUES ---
     foursquareApi.venues = (function () {
-      var promise,
-        deferred = $q.defer();
+      var promise;
 
       var venues = {};
 
       venues.searchVenue = function (nearLocation, searchQuery) {
+        var deferred = $q.defer();
 
-        var getParams = apiConfig.baseParams;
+        var getParams = apiConfig.baseParams();
         getParams.near = nearLocation;
 
         var searchType = 'explore';
@@ -37,9 +39,9 @@ angular.module('testApp')
         promise = promise || $http.get(apiConfig.domain + '/venues/' + searchType, { params: getParams });
         promise
           .success(function (data) {
-            if (data.response && data.response.venues) {
-              var allFoursqareVenues = data.response.venues;
-              deferred.resolve(allFoursqareVenues);
+            console.log('CALL SUCCEED: ', data);
+            if (data.response) {
+              deferred.resolve(data.response);
             } else {
               deferred.reject();
             }
