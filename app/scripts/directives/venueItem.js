bc30e68625e5d7ac75188200b86296f90878ab1d
Vladimirs Matusevics <vladimir.matusevic@gmail.com>
2015-08-07 00:15:13 +0100

    Know that it's a bit late, but I've added some styling for better look and fill. Also added map and link. ( ~ 40 min spent )

--- app/scripts/directives/venueItem.js
+++ app/scripts/directives/venueItem.js
@@ -3,9 +3,14 @@ angular.module('testApp')
   .directive('venueItem', function ($compile) {
     var venueItem =
       '<div class="venue-item">' +
+      '<div class="item-info">' +
       '<span class="title">{{content.name}}</span>' +
-      '<span class="rating" ng-show="content.rating > 0"> ({{content.rating}})</span>' +
+      '<div class="rating" ng-show="content.rating > 0">Rating: <span class="rating-stars">{{content.rating}}</span> based on {{content.ratingSignals}} users</div>' +
       '<div class="checkins">Checkins: {{content.stats.checkinsCount}}</div>' +
+      '</div>' +
+      '<a class="map" href="http://maps.google.com/?q={{content.location.lat}},{{content.location.lng}}" target="_blank">' +
+      '<img src="http://maps.googleapis.com/maps/api/staticmap?center={{content.location.lat}},{{content.location.lng}}&zoom=15&size=320x320&sensor=false">' +
+      '</a>' +
       '</div>';
 
     var linker = function (scope, element) {
